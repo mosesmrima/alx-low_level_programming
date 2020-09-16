35b3382415a68a8f3e74d066ecd989e91f753572
mosesmrima <mrimamss@gmail.com>
2020-09-16 15:11:37 +0300

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
 int main(void)
+
 {
-	char s[] = "\"Programming is like building a multilingual puzzle";
-	puts(s);
-	return (0);
+
+char *s = "\"Programming is like building a multilingual puzzle";
+
+puts(s);
+
+return (0);
 }
