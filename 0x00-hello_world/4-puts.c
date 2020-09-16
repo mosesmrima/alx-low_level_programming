4f5812e4217cd87748480087bf41b0464a1fa184
mosesmrima <mrimamss@gmail.com>
2020-09-16 15:09:47 +0300

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	char s[] = "\"Programming is like building a multilingual puzzle";
+	puts(s);
 	return (0);
 }
