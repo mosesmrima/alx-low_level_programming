091c289bc1f1da66a625dfd18006d574a6d60fc8
mosesmrima <mrimamss@gmail.com>
2020-09-17 12:09:23 +0300

    if..elseif

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -16,15 +16,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-		printf("%li is positive\n", n);
+		printf("%d is positive\n", n);
 	}
 	else if (n == 0)
 	{
-		printf("%li is zero\n", n);
+		printf("%d is zero\n", n);
 	}
 	else
 	{
-		printf("li is negative\n", n);
+		printf("%d is negative\n", n);
 	}
 
 	return (0);
