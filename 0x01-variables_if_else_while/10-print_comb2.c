d348048af9851cf24c8be39c9a7568e500a861f3
mosesmrima <mrimamss@gmail.com>
2020-09-17 16:50:09 +0300

    second combo

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ *main - entry point
+ *Description: combo 2
+ *Return: 0
+ */
+
+int main(void)
+{
+	int i = 48;
+
+	while (i < 58)
+	{
+		int x = 48;
+
+		while (x < 58)
+		{
+			putchar(i);
+			putchar(x);
+			putchar(44);
+			putchar(32);
+			x++;
+		}
+		i++;
+	}
+	return (0);
+}
