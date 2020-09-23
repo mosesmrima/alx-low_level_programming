f29abc90bd2abeb08e24cc72ea307b63efb2c581
mosesmrima <mrimamss@gmail.com>
2020-09-23 09:36:08 +0300

    prints holberton

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -5,17 +5,18 @@
  *Description: _putchar
  *Return: 0
  */
-
 int main(void)
 {
-	char *holberton = "Holberton";
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
 
-	int i = 1;
-
-	while (holberton[i] != '\0')
-	{
-		_putchar(holberton[i]);
-	}
-			_putchar('\n');
-		return (0);
+	return (0);
 }
