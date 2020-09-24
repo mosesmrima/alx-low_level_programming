4aebaab19b601d9c2eae705cd628aaaeb40c85b6
mosesmrima <mrimamss@gmail.com>
2020-09-24 11:12:08 +0300

    header file for this project

---
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -0,0 +1,20 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+/*
+ *file: holberton.h
+ *Author: Moses Mrima
+ *Desc: Header file for all functions used in this project
+ */
+
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+
+#endif
