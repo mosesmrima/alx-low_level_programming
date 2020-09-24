46bc744c7832653337627ac07d4882b07ea05be1
mosesmrima <mrimamss@gmail.com>
2020-09-24 13:36:02 +0300

    print range except 2 values

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ *print_numbers - print from 1 to 9 \
+ *except 2 and 4
+ *
+ */
+
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		if (i != 50 && i != 52)
+		{
+			_putchar (i);
+		}
+	}
+	_putchar('\n');
+}
