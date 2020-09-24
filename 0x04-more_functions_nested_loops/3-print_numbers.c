ebef70d5df650c5f3ecf1cb65129a825f08e1ce4
mosesmrima <mrimamss@gmail.com>
2020-09-24 12:16:03 +0300

    print numbers 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ *print_numbers - prits numbers from 1-9
+ *Return: number
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i <= 57; i++)
+	{
+		_putchar (i);
+	}
+
+	_putchar('\n');
+}
