05c658c69e96024c7ea579b4425e752a829b5069
mosesmrima <mrimamss@gmail.com>
2020-09-28 13:44:25 +0300

    update var usng pointer param

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ *reset_to_98 - function that updates a variable using a pointer
+ *Desc: updates avar to 98
+ *@n: param
+ *Return: void
+ */
+
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
