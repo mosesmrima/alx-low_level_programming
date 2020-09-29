057bcea093d839149b1f6b95db10ac9071fc448f
mosesmrima <mrimamss@gmail.com>
2020-09-29 20:16:04 +0300

    string copy function

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,13 +1,13 @@
 #include "holberton.h"
 
 /**
- *_strcpy - copy string function
- *@dest: param 1
- *@src: param 2
- *Return: pointer
+ * _strcpy - Copies a string pointed to by @src, including the
+ *           terminating null byte, to a buffer pointed to by @dest.
+ * @dest: A buffer to copy the string to.
+ * @src: The source string to copy.
+ *
+ * Return: A pointer to the destination string @dest.
  */
-
-
 char *_strcpy(char *dest, const char *src)
 {
 	int index = 0;
