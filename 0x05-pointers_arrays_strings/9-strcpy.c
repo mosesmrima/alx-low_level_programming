56d4f38be1ad1e5511ae7f1b1327890995fe9cf1
mosesmrima <mrimamss@gmail.com>
2020-09-29 19:36:17 +0300

    copy string functin

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -8,14 +8,15 @@
  */
 
 
-char *_strcpy(char *dest, char *src)
+char *_strcpy(char *dest, const char *src)
 {
-	int i = 0;
+	int index = 0;
 
-	while (src[i])
+	while (src[index])
 	{
-		dest[i] = src[i];
-		i++;
+		dest[index] = src[index];
+		index++;
 	}
+
 	return (dest);
 }
