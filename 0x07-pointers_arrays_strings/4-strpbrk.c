880a7b3d84fcd68ac6478047b7fe195ed09e85d1
mosesmrima <mrimamss@gmail.com>
2020-10-05 20:44:57 +0300

    strpbrk implimetation

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,6 +1,12 @@
 #include "holberton.h"
 
 /**
+ *_strpbrk - locates the first occurrence in the string s of any of
+ *the bytes in
+ *the string accept.
+ *@s: string to pick char
+ *@accept: string to compare to
+ *Return: string pointer
  */
 
 char *_strpbrk(char *s, char *accept)
