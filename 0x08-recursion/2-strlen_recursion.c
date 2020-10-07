de91b82aee00fe67467bdcbac2921787107fdab7
mosesmrima <mrimamss@gmail.com>
2020-10-07 14:21:04 +0300

    strlen with recursion

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -9,13 +9,11 @@
 
 int _strlen_recursion(char *s)
 {
-	int count = 0;
-
-	if (*s)
+	if (!(*s))
 	{
-		count++;
-		count += _strlen_recursion(s + 1);
+		return (0);
 	}
 
-	return (count);
+	else
+		return (1 + _strlen_recursion(s + 1));
 }
