01dd085377734898126c6f19806a0c1be3561921
mosesmrima <mrimamss@gmail.com>
2020-10-07 13:44:43 +0300

    print string in reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ *_print_rev_recursion - print a string in reverse
+ *@s: the string
+ *Return: void
+ */
+
+void _print_rev_recursion(char *s)
+{
+	while (*s)
+	{
+		_print_rev_recursion(++s);
+		_putchar(*s);
+	}
+
+	putchar('\n');
+}
