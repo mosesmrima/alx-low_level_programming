2dd96fcb19deeb5cfc0e2e87954071dd1c26d7ba
mosesmrima <mrimamss@gmail.com>
2020-10-11 19:12:15 +0300

    prints number of passed cmdl arguments

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- *main: prints the number of passed commandline arguments
+ *main - prints the number of passed commandline arguments
  *@argc: the number of arguments
  *@argv: an array of the arguments
  *Return: 0 on success
