d70eb66ee95111bd907ef8d95fc59547257f1050
mosesmrima <mrimamss@gmail.com>
2020-10-13 14:22:52 +0300

    create and init an array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,32 +1,23 @@
-#include "holberton.h"
 #include <stdlib.h>
+#include "holberton.h"
 /**
- *create_array - function createes an array and initialize it
+ *create_array - creates an array
  *@size: size of the array
- *@c:  the char
- *Return: pointer of null
+ *@c: char to initialize the array
+ *Return: array
  */
 
 char *create_array(unsigned int size, char c)
 {
-	if (size == 0)
-	{
-		return (NULL);
-	}
+	int i;
 
 	char *array = malloc(sizeof(char) * size);
 
 	if (array == NULL)
-	{
 		return (NULL);
-	}
-
-	int i;
 
 	for (i = 0; i < size; i++)
-	{
 		array[i] = c;
-	}
 
 	return (array);
 }
