a08da1e15087e1e81eacced3474dcd51356fdb21
mosesmrima <mrimamss@gmail.com>
2020-10-13 18:10:50 +0300

    strdup implimentation

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,13 +10,15 @@ char *_strdup(char *str)
 {
 	int i, len = 0;
 
+	char *str2;
+
 	if (str == NULL)
 		return (NULL);
 
 	for (i = 0; str[i]; i++)
 		len++;
 
-	char *str2 = malloc(sizeof(char) * (len + 1));
+	str2 = malloc(sizeof(char) * (len + 1));
 
 	if (str2 == NULL)
 		return (NULL);
