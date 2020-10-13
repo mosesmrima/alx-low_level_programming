d8d2b0ac39cbf2d3df9b85f9892398105d33d236
mosesmrima <mrimamss@gmail.com>
2020-10-13 14:05:40 +0300

    header file

--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -1,5 +1,9 @@
 #ifndef HFILE_H
 #define HFILE_H
+/*
+ *header file for all functions used
+ */
+
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
