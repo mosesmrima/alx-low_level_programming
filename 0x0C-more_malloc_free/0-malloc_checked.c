08732d28d7be008a0925b16f9e7d20ca7a2716a1
mosesmrima <mrimamss@gmail.com>
2020-10-15 12:34:15 +0300

    malloc with error check

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ *malloc_checked - allocates memory and error checks
+ *@b: size of the memory to allocate
+ *Return: void on success, exit98 on fail
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *alloc;
+
+	alloc = malloc(b);
+	if (alloc == NULL)
+		exit(98);
+}
