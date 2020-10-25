ed6ffd99c6cc58cc30ee68f2b79aa70e7f990216
mosesmrima <mrimamss@gmail.com>
2020-10-25 09:31:13 +0300

    prints numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,7 +14,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list valist;
 	unsigned int i;
 
-	va_start (valist, n);
+	va_start(valist, n);
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(valist, int));
@@ -24,4 +24,5 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	}
 
 	printf("\n");
+	va_end(valist);
 }
