74588d41348e810937c7b71705ff958a6054c726
mosesmrima <mrimamss@gmail.com>
2020-11-02 13:16:09 +0300

    free memrory for linked list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,16 +1,21 @@
 #include "lists.h"
 #include <stdlib.h>
 
+/**
+ *free_list - free memory for linked list
+ *@head: head of the list
+ *Return: void
+ */
 
 void free_list(list_t *head)
 {
-    list_t *temp;
+	list_t *temp;
 
-    while (head -> next != NULL)
-    {
-        temp = head -> next;
-        free (head ->str);
-        free (head);
-        head = temp;
-    }
+	while (head->next != NULL)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
 }
