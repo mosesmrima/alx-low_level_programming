afd70a277f2ca2b948e17a429c32501950c59212
mosesmrima <mrimamss@gmail.com>
2020-11-02 13:07:07 +0300

    add node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -27,7 +27,7 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	}
 
-	new_node->str = strdup(str);
+	new_node->str = str_buff;
 	new_node->len = strlen(str_buff);
 	new_node->next = *head;
 	*head = new_node;
