ef3d0c754e01dcbd1b7867ffc26e55a1d7c547f0
mosesmrima <mrimamss@gmail.com>
2021-07-09 00:09:11 +0300

    print a hash table

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - print a hashtable
+ * @ht: always void
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+
+	unsigned long i;
+	hash_node_t *tmp;
+	int flag;
+
+	flag = 0;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+
+		tmp = ht->array[i];
+		while (tmp != NULL)
+		{
+			if (flag == 1)
+				printf(" ,");
+			printf("%s: %s", tmp->key, tmp->value);
+			flag = 1;
+			tmp = tmp->next;
+		}
+	}
+	printf("}\n");
+}
