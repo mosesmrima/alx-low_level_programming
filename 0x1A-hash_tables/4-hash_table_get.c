281c65cc45b25e996563d04bfba317b079846572
mosesmrima <mrimamss@gmail.com>
2021-07-08 23:06:52 +0300

    find a value associated with key

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - retrieves a value associated with a key.
+ * @ht: the hashtable to lookup
+ * @key: the key to search
+ * Return:  on success - the value
+ *	     otherwise - NULL
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long index;
+	hash_node_t *tmp;
+
+	index = key_index((const unsigned char *)key, ht->size);
+	tmp = ht->array[index];
+	while (tmp != NULL)
+	{
+		if (strcmp(key, tmp->key) == 0)
+		{
+			return (tmp->value);
+		}
+		tmp = tmp->next;
+	}
+
+
+	return (NULL);
+}
