9f31ddc62b1342404488c5446379d0a1b8fb0eda
mosesmrima <mrimamss@gmail.com>
2021-07-08 23:12:18 +0300

    handle NULL parameters

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -12,8 +12,12 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long index;
 	hash_node_t *tmp;
 
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
+
 	index = key_index((const unsigned char *)key, ht->size);
 	tmp = ht->array[index];
+
 	while (tmp != NULL)
 	{
 		if (strcmp(key, tmp->key) == 0)
